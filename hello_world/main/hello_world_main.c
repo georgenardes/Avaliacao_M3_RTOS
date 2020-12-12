5650590fe4a6ccec7567e9761f8c0e3b96b391d9
georgenardes <georgenardes@gmail.com>
2020-12-12 12:42:58 -0300

    implementado semaphore e suspensão de thread

--- hello_world/main/hello_world_main.c
+++ hello_world/main/hello_world_main.c
@@ -13,10 +13,14 @@ Modificado em 02 de dezembro de 2020
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/semphr.h"
 #include "esp_system.h"
 #include "driver/gpio.h"
 #include "nvs_flash.h"
- 
+
+// NUM máximo de produto
+#define NUM_MAX_PROD 1500
+
  // periodo entre passagem de produtos nas esteiras 
 #define TEMPO_EST_1 1000
 #define TEMPO_EST_2 500
@@ -30,6 +34,11 @@ Modificado em 02 de dezembro de 2020
 #define PESO_EST_2 2.0
 #define PESO_EST_3 0.5
 
+// variavel do semaforo
+SemaphoreHandle_t mutual_exclusion_mutex;
+
+// handle de task para suspender
+TaskHandle_t handler_display;
 
 static int num_produtos = 0;
 static float pesos[1500] = {0x0};
@@ -37,7 +46,8 @@ static float pesos[1500] = {0x0};
 void soma_pesos()
 {
     // suspender as demais tasks
-    
+    vTaskSuspend(handler_display);
+
     float peso_total = 0;
     
     // criar threads para realizar a soma paralelamente
@@ -48,12 +58,18 @@ void soma_pesos()
 
     printf("O peso total dos prodos foi %f \n", peso_total);    
 
+    vTaskDelay(10000 / portTICK_RATE_MS);
+
     // retornar as demais tasks
+    vTaskResume(handler_display);
+	
 }
 
 void soma_produto(float peso)
 {
     // mutex (semaforo)
+    xSemaphoreTake(mutual_exclusion_mutex, portMAX_DELAY);
+
     pesos[num_produtos] = peso;
     num_produtos++;
 
@@ -62,7 +78,9 @@ void soma_produto(float peso)
         soma_pesos(); 
         num_produtos = 0;
     }
+
     // end mutex
+    xSemaphoreGive(mutual_exclusion_mutex);
 }
 
 void esteira_1(void *pvParameter)
@@ -138,10 +156,18 @@ void app_main()
 {
     nvs_flash_init();
 
+    // inicializa semáforo
+    mutual_exclusion_mutex = xSemaphoreCreateMutex();
+
+    if( mutual_exclusion_mutex == NULL ){
+        printf("Erro na criação do mutex\n");
+        exit(0);
+    }
+
     xTaskCreate(&esteira_1, "esteira_1", 2048, NULL, 5, NULL);
     xTaskCreate(&esteira_2, "esteira_2", 2048, NULL, 5, NULL);
     xTaskCreate(&esteira_3, "esteira_3", 2048, NULL, 5, NULL);
-    xTaskCreate(&display, "display", 2048, NULL, 5, NULL);
+    xTaskCreate(&display, "display", 2048, NULL, 5, &handler_display);
     // criar task para monitoramento do botão de parada
 
 }
